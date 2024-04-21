4a6d5a22caf162f9799491db9a574bcfa1efb356
shadow2560 <24191064+shadow2560@users.noreply.github.com>
2024-04-21 22:25:16 +0200

    Update symblinks creation/deletion for Retro-go compilation.
    Signed-off-by: shadow2560 <24191064+shadow2560@users.noreply.github.com>

--- _installer/resources/game-and-watch-retro-go/_make_links.cmd
+++ _installer/resources/game-and-watch-retro-go/_make_links.cmd
@@ -1,4 +1,4 @@
 @echo off
 mklink /D retro-go-stm32\nofrendo-go\components\nofrendo\cpu\build %CD%\build 1>NUL
 mklink /D prosystem-go\Core\build %CD%\build 1>NUL
-mklink /D %CD:~0,3%tmp C:\msys64\tmp 1>NUL
+mklink /D %CD:~0,3%tmp ..\msys2\tmp 1>NUL
