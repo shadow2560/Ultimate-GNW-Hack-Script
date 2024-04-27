39b3b079192fb7cc85024e2d060e76ad46a5c56e
shadow2560 <24191064+shadow2560@users.noreply.github.com>
2024-04-27 13:22:20 +0200

    Symblinks creation/deletion reactivated, environement install also install Gnwmanager witch is necessary for some updated project and for futur Retro-go update.
    Signed-off-by: shadow2560 <24191064+shadow2560@users.noreply.github.com>

--- _installer/resources/game-and-watch-retro-go/_make_links.cmd
+++ _installer/resources/game-and-watch-retro-go/_make_links.cmd
@@ -1,4 +1,4 @@
 @echo off
 mklink /D retro-go-stm32\nofrendo-go\components\nofrendo\cpu\build "%CD%\build" 1>NUL
 mklink /D prosystem-go\Core\build "%CD%\build" 1>NUL
-mklink /D ..\tmp "%CD%\..\msys2\tmp" 1>NUL
+mklink /D ..\tmp "%~1\msys2\tmp" 1>NUL
