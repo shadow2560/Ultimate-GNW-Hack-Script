0c108098e36f48fc1d6849ccc9448ee56d6f8d5f
shadow2560 <24191064+shadow2560@users.noreply.github.com>
2025-11-05 04:30:46 +0100

    Update the script used to pull or clone projects linked to this script, now it has a function to make the clone or pull process and the projects are always forced to sync with the remote project.
    Signed-off-by: shadow2560 <24191064+shadow2560@users.noreply.github.com>

--- _installer/pull_repos.sh
+++ _installer/pull_repos.sh
@@ -1,118 +1,80 @@
 S_DIR=$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)
 cd $S_DIR
 
+update_or_clone_project() {
+    local project_name="$1"
+    local folder="$2"
+    local project_address="$3"
+    local branch="$4"
+
+    echo "--------------- $project_name ----------------------------------------------"
+    #rm -r ./../$folder
+    if [ -d "./../$folder" ]; then
+        cd "./../$folder" || exit 1
+        git fetch --all --recurse-submodules
+        local effective_branch="$branch"
+        if [ -z "$effective_branch" ]; then
+            effective_branch=$(git symbolic-ref refs/remotes/origin/HEAD 2>/dev/null | sed 's@^refs/remotes/origin/@@')
+        fi
+        git reset --hard "origin/$effective_branch"
+        git submodule update --init --recursive
+        cd "$S_DIR" || exit 1
+    else
+        if [ -n "$branch" ]; then
+            git clone --recurse-submodules -b "$branch" --single-branch "$project_address" "./../$folder"
+        else
+            git clone --recurse-submodules "$project_address" "./../$folder"
+        fi
+    fi
+    cp -r "./resources/$folder/"* "./../$folder/"
+}
+
 echo "Installing or updating repositories"
 echo ""
 
-echo "--------------- game-and-watch-backup ----------------------------------------------"
-folder=game-and-watch-backup
-#rm -r ./../$folder
-if [ -d "./../$folder" ]; then
-	cd ./../$folder
-	git pull --recurse-submodules
-	# git submodule update --init --recursive --remote --force
-	cd $S_DIR
-else
-    git clone --recurse-submodules https://github.com/ghidraninja/game-and-watch-backup.git ./../$folder
-	# cd ./../$folder
-	# git submodule update --init --recursive --remote --force
-	# cd $S_DIR
-fi
-cp -r ./resources/$folder/* ./../$folder/
+update_or_clone_project \
+    "game-and-watch-backup" \
+    "game-and-watch-backup" \
+    "https://github.com/ghidraninja/game-and-watch-backup.git"
 
 echo ""
 
-echo "--------------- game-and-watch-patch -----------------------------------------------"
-folder=game-and-watch-patch
-#rm -r ./../$folder
-if [ -d "./../$folder" ]; then
-	cd ./../$folder
-	git pull --recurse-submodules
-	# git submodule update --init --recursive --remote --force
-	cd $S_DIR
-else
-	# git clone --recurse-submodules https://github.com/BrianPugh/game-and-watch-patch.git ./../$folder
-	git clone --recurse-submodules https://github.com/shadow2560/game-and-watch-patch.git ./../$folder
-	# cd ./../$folder
-	# git submodule update --init --recursive --remote --force
-	# cd $S_DIR
-fi
-cp -r ./resources/$folder/* ./../$folder/
+update_or_clone_project \
+    "game-and-watch-patch" \
+    "game-and-watch-patch" \
+    "https://github.com/shadow2560/game-and-watch-patch.git"
 
 echo ""
 
-echo "--------------- game-and-watch-patch-old_method -----------------------------------------------"
-folder=game-and-watch-patch-old_method
-#rm -r ./../$folder
-if [ -d "./../$folder" ]; then
-	cd ./../$folder
-	git pull --recurse-submodules
-	# git submodule update --init --recursive --remote --force
-	cd $S_DIR
-else
-	git clone --recurse-submodules -b old_method --single-branch https://github.com/shadow2560/game-and-watch-patch.git ./../$folder
-	# cd ./../$folder
-	# git submodule update --init --recursive --remote --force
-	# cd $S_DIR
-fi
-cp -r ./resources/$folder/* ./../$folder/
+update_or_clone_project \
+    "game-and-watch-patch-old_method" \
+    "game-and-watch-patch-old_method" \
+    "https://github.com/shadow2560/game-and-watch-patch.git" \
+    "old_method"
 
 echo ""
 
-echo "--------------- game-and-watch-retro-go --------------------------------------------"
-folder=game-and-watch-retro-go
-#rm -r ./../$folder
-if [ -d "./../$folder" ]; then
-	cd ./../$folder
-	git pull --recurse-submodules
-	# git submodule update --init --recursive --remote --force
-	cd $S_DIR
-else
-	# git clone --recurse-submodules https://github.com/shadow2560/game-and-watch-retro-go.git ./../$folder
-	# git clone --recurse-submodules -b filesystem --single-branch https://github.com/shadow2560/game-and-watch-retro-go.git ./../$folder
-	git clone --recurse-submodules -b filesystem_wip --single-branch https://github.com/shadow2560/game-and-watch-retro-go.git ./../$folder
+update_or_clone_project \
+    "game-and-watch-retro-go" \
+    "game-and-watch-retro-go" \
+    "https://github.com/shadow2560/game-and-watch-retro-go.git" \
+    "filesystem_wip"
+
 	# git clone --recurse-submodules https://github.com/sylverb/game-and-watch-retro-go.git ./../$folder
-	# git clone --recurse-submodules -b filesystem --single-branch https://github.com/sylverb/game-and-watch-retro-go.git ./../$folder
+	# git clone --recurse-submodules -b filesystem_wip --single-branch https://github.com/sylverb/game-and-watch-retro-go.git ./../$folder
 	 # git clone --recurse-submodules https://github.com/marian-m12l/game-and-watch-retro-go.git ./../$folder
 	# git clone --recurse-submodules https://github.com/BenjaminSoelberg/game-and-watch-retro-go.git ./../$folder
-	# cd ./../$folder
-	# git submodule update --init --recursive --remote --force
-	# cd $S_DIR
-fi
-cp -r ./resources/$folder/* ./../$folder/
 
 echo ""
 
-echo "--------------- game-and-watch-zelda3 ----------------------------------------------"
-folder=game-and-watch-zelda3
-#rm -r ./../$folder
-if [ -d "./../$folder" ]; then
-	cd ./../$folder
-	git pull --recurse-submodules
-	# git submodule update --init --recursive --remote --force
-	cd $S_DIR
-else
-    git clone --recurse-submodules https://github.com/marian-m12l/game-and-watch-zelda3.git ./../$folder
-	# cd ./../$folder
-	# git submodule update --init --recursive --remote --force
-	# cd $S_DIR
-fi
-cp -r ./resources/$folder/* ./../$folder/
+update_or_clone_project \
+    "game-and-watch-zelda3" \
+    "game-and-watch-zelda3" \
+    "https://github.com/marian-m12l/game-and-watch-zelda3.git"
 
 echo ""
 
-echo "--------------- game-and-watch-SMW ----------------------------------------------"
-folder=game-and-watch-smw
-#rm -r ./../$folder
-if [ -d "./../$folder" ]; then
-	cd ./../$folder
-	git pull --recurse-submodules
-	# git submodule update --init --recursive --remote --force
-	cd $S_DIR
-else
-    git clone --recurse-submodules https://github.com/marian-m12l/game-and-watch-smw.git ./../$folder
-	# cd ./../$folder
-	# git submodule update --init --recursive --remote --force
-	# cd $S_DIR
-fi
-cp -r ./resources/$folder/* ./../$folder/
+update_or_clone_project \
+    "game-and-watch-smw" \
+    "game-and-watch-smw" \
+    "https://github.com/marian-m12l/game-and-watch-smw.git"
